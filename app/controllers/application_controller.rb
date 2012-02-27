3a9660ae7d88b473e0eb6cc5e4b4422f19f76045
Gabriella Spain <gspain@greanetree.com>
2012-02-27 18:43:02 -0500

    added login current user back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def current_user
-      #@current_user ||= User.find_by_id(session[:user_id])
-      @current_user = User.find(1)
+      @current_user ||= User.find_by_id(session[:user_id])
     end
 
     def signed_in?
