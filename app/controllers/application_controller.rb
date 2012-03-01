28beb69927cd8b208e694bbe91f0b6c6f2dc748c
Gabriella Spain <gspain@greanetree.com>
2012-03-01 17:08:55 -0500

    removed no-user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,7 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user
-      #@current_user ||= User.find_by_id(session[:user_id])
-      @current_user ||= User.find(1)
+      @current_user ||= User.find_by_id(session[:user_id])
     end
 
     def signed_in?
