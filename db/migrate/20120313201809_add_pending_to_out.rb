fed17941551fcbd465fb272feaecef8e42462514
Joseph Payette Jr <jpayette@greanetree.com>
2012-03-15 13:42:01 -0400

    Changing the column type for DB

--- db/migrate/20120313201809_add_pending_to_out.rb
+++ db/migrate/20120313201809_add_pending_to_out.rb
@@ -1,5 +1,5 @@
 class AddPendingToOut < ActiveRecord::Migration
   def change
-    add_column :outs, :pending, :boolean, :default => 1
+    add_column :outs, :pending, :boolean, :default => false
   end
 end
