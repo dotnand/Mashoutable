ae6d56eb2554d649abee7908eeed4f4acfcf3148
Joseph Payette Jr <jpayette@greanetree.com>
2012-03-17 17:22:38 -0400

    Removing debugging code from Authorization controller

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,16 +1,11 @@
 class AuthorizationController < ApplicationController
   def create  
-    begin
-      auth = request.env['omniauth.auth']
-      logger.info auth.to_json
-      unless @auth = Authorization.find_from_hash(auth)
-        @auth = Authorization.create_from_hash(auth, current_user)
-      end
-
-      self.current_user = @auth.user
-      redirect_to dashboard_path
-    rescue Exception => ex
-      logger.info ex.to_s
+    auth = request.env['omniauth.auth']
+    unless @auth = Authorization.find_from_hash(auth)
+      @auth = Authorization.create_from_hash(auth, current_user)
     end
+
+    self.current_user = @auth.user
+    redirect_to dashboard_path
   end
 end
