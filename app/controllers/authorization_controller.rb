15b38533fc9718bfabd70f0be0467f4a1e1bc6d6
Joseph Payette Jr <jpayette@greanetree.com>
2012-03-17 17:45:51 -0400

    Debugging for Auth on Heroku

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,6 +1,9 @@
 class AuthorizationController < ApplicationController
   def create  
     auth = request.env['omniauth.auth']
+    render :json => auth.to_json
+    return
+    
     unless @auth = Authorization.find_from_hash(auth)
       @auth = Authorization.create_from_hash(auth, current_user)
     end
