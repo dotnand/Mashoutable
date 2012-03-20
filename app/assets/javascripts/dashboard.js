62e3a8c0d7818037bc0e197e8561bc0363a5f3d9
Joseph Payette Jr <jpayette@greanetree.com>
2012-03-20 17:20:16 -0400

    Integrated youtube video content message

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -29,7 +29,7 @@ function bindSendButtonClick(buttonId, outId, outTargetId, formId) {
     });
 }
 
-function bindYoutubeHelpText(buttonId, targetId) {
+function bindYouTubeHelpText(buttonId, targetId) {
     $(buttonId).click(function() {
         $(targetId).toggle();
     });
