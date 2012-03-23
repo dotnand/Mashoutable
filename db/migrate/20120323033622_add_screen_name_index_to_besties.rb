21df119c3791458e3b03cd83d4a1e9aa01188454
Joseph Payette Jr <jpayette@greanetree.com>
2012-03-23 19:02:21 -0400

    Adjusting uniqueness constraint

--- db/migrate/20120323033622_add_screen_name_index_to_besties.rb
+++ db/migrate/20120323033622_add_screen_name_index_to_besties.rb
@@ -1,6 +1,6 @@
 class AddScreenNameIndexToBesties < ActiveRecord::Migration
   def change
     remove_index :besties, :name => 'index_besties_on_screen_name'
-    add_index :besties, :screen_name, :name => 'besties_screen_name_idx', :unique => true
+    add_index :besties, [:screen_name, :user_id], :name => 'besties_screen_name_idx', :unique => true
   end
 end
