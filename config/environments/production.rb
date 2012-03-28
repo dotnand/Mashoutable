32851f54bcdfe9c50d78da185cfc0b6817026a35
Joseph Payette Jr <jpayette@greanetree.com>
2012-03-28 18:19:54 -0400

    Change email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,5 @@ Mashoutable::Application.configure do
     :port                 => ENV['EMAIL_PORT'],
     :user_name            => ENV['EMAIL_USER_NAME'],
     :password             => ENV['EMAIL_PASSWORD'],
-    :authentication       => :plain,
-    :enable_starttls_auto => true }
+    :authentication       => :plain }
 end
