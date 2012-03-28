dbbe686454da3ff3e8e7ed55a03782b040e467fa
Joseph Payette Jr <jpayette@greanetree.com>
2012-03-28 16:15:10 -0400

    Adjusting Uniqueness Validation and Index

--- db/migrate/20120214145055_add_index_to_screen_name_on_bestie_table.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToScreenNameOnBestieTable < ActiveRecord::Migration
-  def change
-    add_index :besties, :screen_name
-  end
-end
--- db/migrate/20120323033622_add_screen_name_index_to_besties.rb
+++ db/migrate/20120323033622_add_screen_name_index_to_besties.rb
@@ -1,6 +1,5 @@
 class AddScreenNameIndexToBesties < ActiveRecord::Migration
   def change
-    remove_index :besties
     add_index :besties, [:screen_name, :user_id], :name => 'besties_screen_name_idx', :unique => true
   end
 end
