c531b6619e070b6368e50ab627a63a880f595875
Joseph Payette Jr <jpayette@greanetree.com>
2012-03-29 10:43:12 -0400

    Using Sendgrid instead of GoDaddy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,12 +59,13 @@ Mashoutable::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => ENV['EMAIL_ADDRESS'],
-    :port                 => ENV['EMAIL_PORT'],
-    :user_name            => ENV['EMAIL_USER_NAME'],
-    :password             => ENV['EMAIL_PASSWORD'],
-    :domain               => 'mashoutable.info',
-    :authentication       => :login}
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
