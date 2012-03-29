9a24c5e4bd237665b0406062c8e231801d9b87c7
Joseph Payette Jr <jpayette@greanetree.com>
2012-03-29 17:53:28 -0400

    Adjusted sendgrid port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Mashoutable::Application.configure do
   
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => '25',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
