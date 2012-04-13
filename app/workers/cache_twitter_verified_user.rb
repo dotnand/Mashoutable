34d7e465e80a4074b4eac0c82f56a2135ae2a6ab
Joseph Payette Jr <jpayette@greanetree.com>
2012-04-13 14:41:03 -0400

    Debugging for production, scheduler

--- app/workers/cache_twitter_verified_user.rb
+++ app/workers/cache_twitter_verified_user.rb
@@ -11,5 +11,8 @@ class CacheVerifiedTwitterUser
 
     VerifiedTwitterUser.remove_verified(remove_list)
     VerifiedTwitterUser.add_verified(add_list)
+    
+    rescue Exception => ex
+      Rails.logger.info ex.to_s
   end
 end
