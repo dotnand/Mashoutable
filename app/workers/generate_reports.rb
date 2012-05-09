740070543bbbcd823a014a158b06a348b9ce8be6
Matthew Gerrior <gerrim@rpi.edu>
2012-05-09 11:53:35 -0400

    Updated daily reports to include all time tallies, and to more accurately count blastouts/mashouts/shoutouts in past 24 hours.

--- app/workers/generate_reports.rb
+++ app/workers/generate_reports.rb
@@ -2,34 +2,43 @@ class GenerateReports
   @queue = :generate_reports_queue
 
   def self.yesterday
-    (Time.now.midnight - 1.day)..(Time.now.midnight)
+    (Time.now.yesterday.midnight)..(Time.now.midnight)
   end
 
-  def self.out_count(user_ids = [])
-    blastouts = Blastout.created_yesterday.where(:user_id => user_ids)
-    mashouts  = Mashout.created_yesterday.where(:user_id => user_ids)
-    shoutouts = Shoutout.created_yesterday.where(:user_id => user_ids)
+  def self.out_count(user_ids, date_range = nil)
+    return [0, 0, 0] if user_ids.empty?
+
+    blastouts = Blastout.where(:user_id => user_ids)
+    mashouts  = Mashout.where(:user_id => user_ids)
+    shoutouts = Shoutout.where(:user_id => user_ids)
+
+    if date_range.present?
+      blastouts = blastouts.where(:created_at => date_range)
+      mashouts  = mashouts.where(:created_at => date_range)
+      shoutouts = shoutouts.where(:created_at => date_range)
+    end
+
+    blastouts = blastouts - blastouts.joins(:out_errors)
+    mashouts  = mashouts  - mashouts.joins(:out_errors)
+    shoutouts = shoutouts - shoutouts.joins(:out_errors)
 
     [blastouts.count, mashouts.count, shoutouts.count]
   end
 
-  def self.build_provider_report_row(provider, user_ids)
-    blastout_count, mashout_count, shoutout_count = out_count(user_ids)
-    { 'Provider' => provider,
-      'User Count' => user_ids.count,
-      'Blastouts' => blastout_count,
-      'Mashouts' => mashout_count,
-      'Shoutouts' => shoutout_count
-    }
+  def self.build_provider_report_row(provider, user_ids, date_range = nil)
+    blastout_count, mashout_count, shoutout_count = out_count(user_ids, date_range)
+    { 'Provider'    => provider,
+      'Blastouts'   => blastout_count,
+      'Mashouts'    => mashout_count,
+      'Shoutouts'   => shoutout_count }
   end
 
   def self.build_out_count_row(status, all, blastouts, mashouts, shoutouts)
-    { 'Status' => status,
-      'All' => all.count,
+    { 'Status'    => status,
+      'All'       => all.count,
       'Blastouts' => blastouts.count,
-      'Mashouts' => mashouts.count,
-      'Shoutouts' => shoutouts.count
-    }
+      'Mashouts'  => mashouts.count,
+      'Shoutouts' => shoutouts.count }
   end
 
   def self.build_out_counts_report
@@ -71,44 +80,77 @@ class GenerateReports
      table
   end
 
-  def self.build_providers_report
-    table                      = Table(['Provider', 'User Count', 'Blastouts', 'Mashouts', 'Shoutouts'])
-    providers                  = [Authorization::FACEBOOK,
-                                  Authorization::TWITTER,
-                                  [Authorization::FACEBOOK,
-                                   Authorization::TWITTER]]
+  def self.build_providers_report(date_range = nil)
+    table                        = Table(['Provider', 'Accounts Created', 'Blastouts', 'Mashouts', 'Shoutouts'])
+    providers                    = [Authorization::FACEBOOK,
+                                    Authorization::TWITTER,
+                                    [Authorization::FACEBOOK,
+                                     Authorization::TWITTER]]
+    users_with_one_authorization = User.joins(:authorizations)
+                                       .select('"users"."id"')
+                                       .group('"users"."id"')
+                                       .having('count(*) = 1')
+                                       .map(&:id)
 
     providers.each do |provider|
       if provider.is_a?(String)
         # Users that authorized this provider
-        authorizations = Authorization.created_yesterday.where(:provider => provider)
-        users          = authorizations.map(&:user)
-        user_ids       = users.map(&:id)
-        provider_name  = provider.titleize
+        authorizations      = Authorization.joins(:user).where(:provider => provider,
+                                                               :users => { :id => users_with_one_authorization })
+        authorized_user_ids = authorizations.map(&:user_id)
+        provider_name       = "#{provider.titleize} Only"
+        report_row          = build_provider_report_row(provider_name,
+                                                        authorized_user_ids,
+                                                        date_range)
 
-        table << build_provider_report_row(provider_name, user_ids)
+        if date_range.present?
+          authorizations_by_date = authorizations.where(:created_at => date_range)
 
-        users = users.select { |user| user.authorizations.count == 1 }
-        user_ids = users.map(&:id)
-        provider_name = provider_name + ' Only'
+          report_row.merge!('Accounts Created' => authorizations_by_date.count)
+        else
+          report_row.merge!('Accounts Created' => authorized_user_ids.count)
+        end
 
-        table << build_provider_report_row(provider_name, user_ids)
+        table << report_row
       else
-        users = []
+        user_ids = []
+
         # Get the users for each provider
         provider.each do |individual_provider|
-          authorizations = Authorization.created_yesterday.where(:provider => individual_provider)
-          users << authorizations.map(&:user)
+          authorizations = Authorization.where(:provider => individual_provider)
+          user_ids << authorizations.map(&:user_id)
         end
+
         # Intersect the users from each provider to get the users that
         # have authorized with all providers in the list
-        users         = users.inject(:&)
-        user_ids      = users.map(&:id)
-        provider_name = provider.map(&:titleize).join(' / ')
+        user_ids      = user_ids.inject(:&)
+        provider_name = provider.map(&:titleize).join(' + ')
+        report_row    = build_provider_report_row(provider_name, user_ids, date_range)
 
-        table << build_provider_report_row(provider_name, user_ids)
+        if date_range.present?
+          user_ids_by_date = []
+
+          provider.each do |individual_provider|
+            authorizations = Authorization.where(:provider => individual_provider, :created_at => date_range)
+            user_ids_by_date << authorizations.map(&:user_id)
+          end
+
+          user_ids_by_date = user_ids_by_date.inject(:&)
+
+          report_row.merge!('Accounts Created' => user_ids_by_date.count)
+        else
+          report_row.merge!('Accounts Created' => user_ids.count)
+        end
+
+        table << report_row
       end
     end
+
+    table << { 'Provider'         => 'Total',
+               'Accounts Created' => table.sum('Accounts Created'),
+               'Blastouts'        => table.sum('Blastouts'),
+               'Mashouts'         => table.sum('Mashouts'),
+               'Shoutouts'        => table.sum('Shoutouts') }
 
     table
   end
@@ -135,20 +177,28 @@ class GenerateReports
       end
     end
 
+    table << { 'Source' => 'Total',
+               'Error'  => nil,
+               'Count'  => table.sum('Count'),
+               'Percent' => nil }
+
     table
   end
 
   def self.perform
     reports = []
 
-    reports << { :filename => "#{Time.now.strftime('%Y%m%d%H%M%S')}_providers_report.csv",
-                 :name     => 'Provider Statistics',
+    reports << { :filename => "#{Time.now.strftime('%Y%m%d%H%M%S')}_providers_report_all_time.csv",
+                 :name     => 'Provider Statistics ( all time )',
                  :table    => build_providers_report }
+    reports << { :filename => "#{Time.now.strftime('%Y%m%d%H%M%S')}_providers_report_24_hours.csv",
+                 :name     => 'Provider Statistics ( last 24 hours )',
+                 :table    => build_providers_report(yesterday) }
     reports << { :filename => "#{Time.now.strftime('%Y%m%d%H%M%S')}_out_counts_report.csv",
-                 :name     => 'Out Statistics',
+                 :name     => 'Out Statistics ( last 24 hours )',
                  :table    => build_out_counts_report }
     reports << { :filename => "#{Time.now.strftime('%Y%m%d%H%M%S')}_errors_report.csv",
-                 :name     => 'Error Statistics',
+                 :name     => 'Error Statistics ( last 24 hours )',
                  :table    => build_errors_report }
 
     ReportMailer.report_email(reports).deliver
