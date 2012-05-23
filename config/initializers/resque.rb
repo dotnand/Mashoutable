f481c307faeba0d2d65cb530e05b44689b3bebb5
Matthew Gerrior <gerrim@rpi.edu>
2012-05-23 14:31:49 -0400

    Resolved 'bad URI(is not URI?)' error when performing 'rake assets:precompile'

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,9 +2,11 @@ require 'resque'
 require 'resque/server'
 require 'resque_scheduler'
 
-uri             = URI.parse(ENV["REDIS_URL"])
-Resque.redis    = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
+begin
+  uri             = URI.parse(ENV["REDIS_URL"])
+  Resque.redis    = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
 
-Dir["#{Rails.root}/app/workers/*.rb"].each { |file| require file }
-
+  Dir["#{Rails.root}/app/workers/*.rb"].each { |file| require file }
+rescue URI::InvalidURIError
+end
